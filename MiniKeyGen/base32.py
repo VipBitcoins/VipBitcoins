a61ba6bfc6a07b06207a153222ddf4496cbeecc3
Vip <vipbitcoins1@gmail.com>
2024-10-30 20:21:51 -0400

    Bitcoin Mini Key Generator
    Here are 3 Bitcoin Mini Key Generators which can be used to generate base57 (30) character private addresses, base32 (30) character private addresses, & base32 (22) character private addresses. These generators are most commonly used in the creation of Physical Bitcoins & are very useful when trying to conserve space.

---
+++ MiniKeyGen/base32.py
@@ -0,0 +1,62 @@
+import random
+import hashlib
+ 
+BASE58 = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ'
+
+def Candidate():
+    srandom = random.SystemRandom()
+    return 'S' + ''.join([BASE58[srandom.randrange(0, len(BASE58))] for _ in range(29)])
+
+def GenerateKeys(numKeys = 1):
+    """
+    Generate mini private keys and output the mini key as well as the full
+    private key. numKeys is The number of keys to generate, and
+    """
+    keysGenerated = 0
+    totalCandidates = 0
+    while keysGenerated < numKeys:
+        try:
+            cand = Candidate()
+            # Do typo check
+            t = '%s?' % cand
+            # Take one round of SHA256
+            candHash = hashlib.sha256(t.encode('utf-8')).digest()
+            # Check if the first eight bits of the hash are 0
+            if candHash[0] == 0:
+                privateKey = GetPrivateKey(cand)
+                print(f'\n{cand}\nSHA256(): {privateKey}\nsha256(?): {candHash.hex()}')
+                if CheckShortKey(cand):
+                    print('Validated.')
+                else:
+                    print('Invalid!')
+                keysGenerated += 1
+            totalCandidates += 1
+        except KeyboardInterrupt:
+            break
+    
+    print(f'\nKeys Generated: {keysGenerated}\nTotal Candidates: {totalCandidates}\nReject Percentage: {100 * (1.0 - keysGenerated / float(totalCandidates)):.1f}')
+
+ 
+def GetPrivateKey(shortKey):
+    """
+    Returns the hexadecimal representation of the private key corresponding
+    to the given short key.
+    """
+    if CheckShortKey(shortKey):
+        return hashlib.sha256(shortKey.encode('utf-8')).hexdigest()
+    else:
+        print('Typo detected in private key!')
+        return None
+ 
+def CheckShortKey(shortKey):
+    """
+    Checks for typos in the short key.
+    """
+    if len(shortKey) != 30:
+        return False
+    t = f'{shortKey}?'
+    tHash = hashlib.sha256(t.encode('utf-8')).digest()
+    # Check to see that first byte is \x00
+    return tHash[0] == 0
+
+GenerateKeys()
